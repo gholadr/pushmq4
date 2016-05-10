f98c7fdd9ed5e20ced7af02aacc75af12544a77e
gholadr <david.rossellat@gmail.com>
2016-05-10 16:29:05 +0700

    removing fabric from MainActivity and playing with observable patterns

--- mqtt/src/main/java/co/ghola/pushmq4/MainActivity.java
+++ mqtt/src/main/java/co/ghola/pushmq4/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         setContentView(R.layout.activity_main);
 
         if(!ServiceUtils.getInstance().serviceIsRunning(getApplicationContext()) && DeviceStatus.isOnline(this)) {
